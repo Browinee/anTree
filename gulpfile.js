f368735213c43f17441b51ade35f4037c006c453
justin <justin@justinde-MBP.Home>
2016-09-17 13:19:06 +0800

    bundle

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var concat     = require('gulp-concat');
 var uglify     = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var rev        = require('gulp-rev');
+var useref     = require('gulp-useref');
+var revreplace = require('gulp-rev-replace');
 gulp.task('build:vendor',function(){
 
    gulp.src([
@@ -12,12 +14,13 @@ gulp.task('build:vendor',function(){
       "node_modules/reflect-metadata/Reflect.js",
       "node_modules/systemjs/dist/system.src.js"
    	])
-   .pipe(concat('vendor.min.js'))
    .pipe(sourcemaps.init())
+   .pipe(useref())
    .pipe(uglify())
    .pipe(rev())
    .pipe(sourcemaps.write())
-   .pipe(gulp.dest('./dist'));
+   .pipe(gulp.dest('./dist'))
+   .pipe(revreplace())
 
 
 });
